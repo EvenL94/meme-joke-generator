0a3229ae77216068d10081abc98c65036b56ccd9
EvanL94 <evanli.careers.ca@gmail.com>
2024-09-05 12:09:09 -0700

    update homepage styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,10 +20,10 @@ export default function Home() {
 
   return (
     <main>
-      <h1 className="text-4xl font-bold text-gray-800 bg-gradient-to-r from-indigo-400 to-blue-500 text-transparent bg-clip-text py-4 px-6 rounded-lg shadow-lg">
-        公式生成器
-      </h1>
       <div className="container mx-auto w-4/5">
+        <h1 className="text-4xl font-bold text-gray-800 bg-gradient-to-r from-indigo-400 to-blue-500 text-transparent bg-clip-text py-4 px-6 rounded-lg shadow-lg">
+          公式生成器
+        </h1>
         <div className="flex">
           <div className="w-1/4  border border-blue-300 rounded-md p-4">
             <p>请选择你的英雄：</p>
