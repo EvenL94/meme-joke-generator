dd1699c67089b9326129f70f5e69d3564da82917
EvanL94 <evanli.careers.ca@gmail.com>
2024-09-05 13:48:21 -0700

    update deploying settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
+  basePath: isProd ? "/meme-joke-generator" : "",
+  assetPrefix: isProd ? "/meme-joke-generator/" : "",
+  images: {
+    unoptimized: true,
+  },
   output: "export",
 };
-
 export default nextConfig;
