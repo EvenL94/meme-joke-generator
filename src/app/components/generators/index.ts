44aacde6c12a2d3452a54d0a3f67a12332694bce
EvanL94 <evanli.careers.ca@gmail.com>
2024-09-05 12:04:37 -0700

    update imports

---
+++ src/app/components/generators/index.ts
@@ -0,0 +1,5 @@
+export { default as Whw } from "./GeneratorWhw";
+export { default as Gi } from "./GeneratorGi";
+export { default as Lfy } from "./GeneratorLfy";
+export { default as Hhk } from "./GeneratorHhk";
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 "use client";
 
 import React, { useState } from "react";
-import GeneratorWhw from "./components/generators/GeneratorWhw";
-import GeneratorGi from "./components/generators/GeneratorGi";
-import GeneratorLfy from "./components/generators/GeneratorLfy";
-import GeneratorHhk from "./components/generators/GeneratorHHK";
+import * as Generators from "./components/generators";
 
 export default function Home() {
   const componentMap = new Map<string, JSX.Element>([
-    ["whw", <GeneratorWhw />],
-    ["gi", <GeneratorGi />],
-    ["lfy", <GeneratorLfy />],
-    ["hhk", <GeneratorHhk />],
+    ["whw", <Generators.Whw />],
+    ["gi", <Generators.Gi />],
+    ["lfy", <Generators.Lfy />],
+    ["hhk", <Generators.Hhk />],
   ]);
 
   const [selectedComponentKey, setSelectedComponentKey] =
@@ -23,6 +20,9 @@ export default function Home() {
 
   return (
     <main>
+      <h1 className="text-4xl font-bold text-gray-800 bg-gradient-to-r from-indigo-400 to-blue-500 text-transparent bg-clip-text py-4 px-6 rounded-lg shadow-lg">
+        公式生成器
+      </h1>
       <div className="container mx-auto w-4/5">
         <div className="flex">
           <div className="w-1/4  border border-blue-300 rounded-md p-4">
@@ -33,16 +33,16 @@ export default function Home() {
               className="block  px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-300 focus:border-indigo-500 text-gray-700 bg-gray-100 hover:bg-gray-200 transition duration-300 ease-in-out"
             >
               <option className="bg-white text-gray-700" value="whw">
-                "我是来玩梗的，你们要干什么!"
+                我是来玩梗的，你们要干什么!
               </option>
               <option className="bg-white text-gray-700" value="gi">
                 你说的对，但是
               </option>
               <option className="bg-white text-gray-700" value="lfy">
-                "老佛爷您的马死了"
+                老佛爷您的马死了
               </option>
               <option className="bg-white text-gray-700" value="hhk">
-                "为什么要演奏春日影?!"
+                为什么要演奏春日影?!
               </option>
             </select>
           </div>
