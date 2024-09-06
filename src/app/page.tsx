e426b06743fc6c1792dd9767e8ebb7bb2f9b0479
EvanL94 <evanli.careers.ca@gmail.com>
2024-09-06 09:44:01 -0700

    add responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,13 +44,13 @@ export default function Home() {
         <h1 className="text-4xl font-bold text-gray-800 bg-gradient-to-r from-indigo-400 to-blue-500 text-transparent bg-clip-text py-4 px-6 rounded-lg shadow-lg">
           公式生成器
         </h1>
-        <div className="flex">
-          <div className="w-1/4  border border-blue-300 rounded-md p-4">
+        <div className="block xl:flex">
+          <div className="w-full xl:w-1/4 border border-blue-300 rounded-md p-4 mb-4 xl:mb-0">
             <p>请选择你的英雄：</p>
             <select
               value={selectedComponentKey}
               onChange={handleSelectChange}
-              className="block  px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-300 focus:border-indigo-500 text-gray-700 bg-gray-100 hover:bg-gray-200 transition duration-300 ease-in-out"
+              className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-300 focus:border-indigo-500 text-gray-700 bg-gray-100 hover:bg-gray-200 transition duration-300 ease-in-out"
             >
               <option className="bg-white text-gray-700" value="whw">
                 我是来玩梗的，你们要干什么!
@@ -66,7 +66,7 @@ export default function Home() {
               </option>
             </select>
           </div>
-          <div className="w-3/4 border border-gray-300 rounded-md p-4">
+          <div className="w-full xl:w-3/4 border border-gray-300 rounded-md p-4">
             {componentMap.get(selectedComponentKey)}
           </div>
         </div>
